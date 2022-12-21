37d6468142755a2218c79368eeb1f3737fd63529
darren wells <xdarebear5x@gmail.com>
2022-12-20 19:54:45 -0700

    axios fix

--- server/services/render.js
+++ server/services/render.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/workouts
-    axios.get('http://localhost:3000/api/workouts')
+    axios.get('https://repulsive-trail-production.up.railway.app/api/workouts')
         .then(function(response){
             res.render('index', {workouts : response.data});
         })
@@ -15,7 +15,7 @@ exports.add_workout = (req, res) => {
 },
 
 exports.update_workout = (req, res) => {
-    axios.get('http://localhost:3000/api/workouts', {params : {id : req.query.id}})
+    axios.get('https://repulsive-trail-production.up.railway.app/api/workouts', {params : {id : req.query.id}})
     .then(function(workoutdata){
         res.render("update_workout", { workout : workoutdata.data})
     })
